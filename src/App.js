498d0d53b1aaf97e23231029cc5238c88601071e
Otyko <otyko81@gmail.com>
2022-01-14 12:08:32 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React, {useEffect} from 'react';
 
+const App = () => {
+    useEffect(()=>{
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => {
+                console.log(value);
+            });
+    },[])
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default App;
